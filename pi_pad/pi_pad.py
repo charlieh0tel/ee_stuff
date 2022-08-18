cbfcb4a34ad4d460e4829947f1b4e8ae43420c79
Christopher Hoover <ch@murgatroid.com>
2022-08-17 17:14:51 -0700

    Cleans up ambiguity between shunt and series components.

--- pi_pad/pi_pad.py
+++ pi_pad/pi_pad.py
@@ -9,41 +9,54 @@ def K(amplitude_db):
     return 10.**(amplitude_db/20)
 
 
-def R1(z0, K):
+def R_shunt(z0, K):
     return z0 * (K + 1.) / (K - 1.)
 
 
-def R2(z0, K):
+def R_series(z0, K):
     return (z0 / 2.) * (K - 1/K)
 
 
-def Z(r1, r2):
+def Z(resistors):
+    r_shunt = resistors['r_shunt']
+    r_series = resistors['r_series']
     return np.reciprocal(np.sqrt(
-        (np.reciprocal(np.square(r1)) + 2. / (r1 * r2))))
+        (np.reciprocal(np.square(r_shunt)) + 2. / (r_shunt * r_series))))
 
 
 def pi_pad(impedance, amplitude_db, series=None):
     k = K(amplitude_db)
-    r1 = R1(impedance, k)
-    r2 = R2(impedance, k)
+    r_shunt = R_shunt(impedance, k)
+    r_series = R_series(impedance, k)
     if series:
-        r1 = resistors.closest_value(r1, series)
-        r2 = resistors.closest_value(r2, series)
-    return (r1, r2)
+        r_shunt = resistors.closest_value(r_shunt, series)
+        r_series = resistors.closest_value(r_series, series)
+    return {'r_shunt': r_shunt, 'r_series': r_series}
 
 
 def dump(impedance=50,
          pad_dbs=[1, 2, 3, 6, 9, 10, 20],
          series=['', 'E24', 'E48', 'E96', 'E192']):
-    print("r1 is shunt, r2 is series")
+    print("                            ")
+    print("          R_series          ")
+    print("  --------/\/\/\/\--------- ")
+    print("      |              |      ")
+    print("      \              \      ")
+    print("      / R_shunt      /      ")
+    print("      \              \      ")
+    print("      |              |      ")
+    print("      _              _      ")
+    print()
     for pad_db in pad_dbs:
-        print(f"{pad_db} dB")
+        print(f"{pad_db} dB:")
         for s in series:
-            (r1, r2) = pi_pad(impedance, pad_db, s)
-            z = Z(r1, r2)
+            resistors = pi_pad(impedance, pad_db, s)
+            r_shunt = resistors['r_shunt']
+            r_series = resistors['r_series']
+            z = Z(resistors)
             z_error = np.abs(z - impedance) / impedance
-            print(f"  {s:4s} r1={r1:.3f} r2={r2:.3f}, Z={z:.3f}, "
-                  f"Z_err={z_error * 100.:.2f} %")
+            print(f"  {s:4s} R_shunt={r_shunt:.3f} R_series={r_series:.3f}, "
+                  f"Z={z:.3f}, Z_err={z_error * 100.:.2f} %")
         print()
 
 
