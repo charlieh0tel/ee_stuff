dee9ad6deb55b02822974e1bb0b330aee9ecb329
Christopher Hoover <ch@murgatroid.com>
2023-02-18 13:01:02 -0800

    print angle in output

--- kicad/junction.py
+++ kicad/junction.py
@@ -7,6 +7,6 @@ def generate(n, radius, origin=(0., 0.), angle_start=0.):
         angle = angle_start + i * dangle;
         pt = (origin[0] + radius * math.cos(math.radians(angle)),
               origin[1] + radius * math.sin(math.radians(angle)))
-        print(f"{origin[0]:f} {origin[1]:f} {pt[0]:f} {pt[1]:f}")
+        print(f"{angle:f} {origin[0]:f} {origin[1]:f} {pt[0]:f} {pt[1]:f}")
     
     return 0
