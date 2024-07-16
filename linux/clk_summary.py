1c7be8b21b76256b896e039c506d52b7db83b0e8
Christopher Hoover <ch@murgatroid.com>
2024-07-16 10:47:20 -0700

    .

--- linux/clk_summary.py
+++ linux/clk_summary.py
@@ -41,14 +41,21 @@ class Clock:
         return clocks
 
 
-all_clocks = Clock.load_summary("clk_summary")
-active_clocks = itertools.filterfalse(lambda c: not c.hardware_enabled, all_clocks)
-active_clocks_sorted_by_rate = sorted(active_clocks, key=lambda c: c.rate)
+def main(argv):
+    assert 1 <= len(argv) <= 2
+    clk_summary_path = argv[1] if len(argv) == 2 else "clk_summary"
+    all_clocks = Clock.load_summary(clk_summary_path)
+    active_clocks = itertools.filterfalse(lambda c: not c.hardware_enabled, all_clocks)
+    active_clocks_sorted_by_rate = sorted(active_clocks, key=lambda c: c.rate)
 
-for k, g in itertools.groupby(active_clocks_sorted_by_rate,
-                              lambda c: c.rate):
-    print(f"{k / 1e6} MHz")
-    #print(f"  (2x={2 * k / 1e6} MHz, 3x={3 * k / 1e6} MHz, 4x={4 * k / 1e6} MHz, 5x={5 * k / 1e6} MHz)")
-    names = ", ".join(map(lambda c: c.name, g))
-    print(textwrap.fill(names, initial_indent="  ", subsequent_indent="  "))
-    print()
+    for k, g in itertools.groupby(active_clocks_sorted_by_rate,
+                                  lambda c: c.rate):
+        print(f"{k / 1e6} MHz")
+        #print(f"  (2x={2 * k / 1e6} MHz, 3x={3 * k / 1e6} MHz, 4x={4 * k / 1e6} MHz, 5x={5 * k / 1e6} MHz)")
+        names = ", ".join(map(lambda c: c.name, g))
+        print(textwrap.fill(names, initial_indent="  ", subsequent_indent="  "))
+        print()
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
