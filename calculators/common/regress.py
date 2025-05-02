fd81ed63eec2b61b69f65a2bf009b160267f4a2a
Christopher Hoover <ch@murgatroid.com>
2025-05-02 16:06:43 -0700

    Fixes t paramers; autopep8

--- calculators/common/regress.py
+++ calculators/common/regress.py
@@ -16,7 +16,7 @@ def ols_linregress(x, y, force_origin=False):
         slope = model.params[0]  # Get slope
         intercept = 0.0         # Intercept is 0
         stderr = model.bse[0]
-        p_value = model.pvalues[0] # Get p-value for slope
+        p_value = model.pvalues[0]  # Get p-value for slope
     else:
         # Add a constant term to x, required by statsmodels
         #  This allows the model to estimate the intercept
@@ -26,20 +26,20 @@ def ols_linregress(x, y, force_origin=False):
         slope = model.params[1]  # Get slope
         intercept = model.params[0]  # Get intercept
         stderr = model.bse[1]
-        p_value = model.pvalues[1] # Get p-value for slope
+        p_value = model.pvalues[1]  # Get p-value for slope
 
     r_value = np.sqrt(model.rsquared)
 
     return slope, intercept, r_value, p_value, stderr
 
 
-def robust_linregress(x, y, a=1.345, force_origin=False):
+def robust_linregress(x, y, force_origin=False, t=1.345):
     x = np.asarray(x)
     y = np.asarray(y)
 
     if force_origin:
         # Perform regression through the origin by not including a constant
-        model = rlm.RLM(y, x, M=HuberT()).fit()
+        model = rlm.RLM(y, x, M=HuberT(t=t)).fit()
         slope = model.params[0]
         intercept = 0.0
         stderr = model.bse[0]
@@ -47,7 +47,7 @@ def robust_linregress(x, y, a=1.345, force_origin=False):
         # Add a constant term to x, required by statsmodels
         x = sm.add_constant(x)
         # Perform the robust regression using Huber's T norm
-        model = rlm.RLM(y, x, M=HuberT()).fit()
+        model = rlm.RLM(y, x, M=HuberT(t=t)).fit()
         slope = model.params[1]
         intercept = model.params[0]
         stderr = model.bse[1]
--- calculators/common/regress_test.py
+++ calculators/common/regress_test.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     y = np.array([2, 4, 5, 4, 5, 8, 9, 10, 12, 30])  # Add an outlier
 
     # Perform ordinary least squares linear regression
-    slope_ols, intercept_ols, r_value_ols, p_value_ols, stderr_ols = ols_linregress(x, y)
+    slope_ols, intercept_ols, r_value_ols, p_value_ols, stderr_ols = ols_linregress(
+        x, y)
 
     print("Ordinary Least Squares Regression Results:")
     print("Slope:", slope_ols)
@@ -18,9 +19,9 @@ if __name__ == '__main__':
     print("P-value:", p_value_ols)
     print("Standard Error of the slope:", stderr_ols)
 
-
     # Perform robust linear regression
-    slope_robust, intercept_robust, r_value_robust, p_value_robust, stderr_robust = robust_linregress(x, y)
+    slope_robust, intercept_robust, r_value_robust, p_value_robust, stderr_robust = robust_linregress(
+        x, y)
 
     print("\nRobust Regression Results:")
     print("Slope:", slope_robust)
@@ -32,8 +33,10 @@ if __name__ == '__main__':
     # Plot the data and the regression lines
     plt.figure()
     plt.scatter(x, y, label='Data with outlier')
-    plt.plot(x, slope_ols * x + intercept_ols, color='red', label='Ordinary Least Squares')
-    plt.plot(x, slope_robust * x + intercept_robust, color='green', label='Robust Regression (HuberT)')
+    plt.plot(x, slope_ols * x + intercept_ols,
+             color='red', label='Ordinary Least Squares')
+    plt.plot(x, slope_robust * x + intercept_robust,
+             color='green', label='Robust Regression (HuberT)')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Comparison of Robust and Ordinary Least Squares Regression')
@@ -42,7 +45,8 @@ if __name__ == '__main__':
     plt.show()
 
     # Example forcing through origin
-    slope_origin_ols, intercept_origin_ols, r_value_origin_ols, p_value_origin_ols, stderr_origin_ols = ols_linregress(x, y, force_origin=True)
+    slope_origin_ols, intercept_origin_ols, r_value_origin_ols, p_value_origin_ols, stderr_origin_ols = ols_linregress(
+        x, y, force_origin=True)
     print("\nOrdinary Least Squares Regression Through Origin:")
     print("Slope:", slope_origin_ols)
     print("Intercept:", intercept_origin_ols)
@@ -50,7 +54,8 @@ if __name__ == '__main__':
     print("P-value:", p_value_origin_ols)
     print("Standard Error of the slope:", stderr_origin_ols)
 
-    slope_origin_robust, intercept_origin_robust, r_value_origin_robust, p_value_origin_robust, stderr_origin_robust = robust_linregress(x, y, force_origin=True)
+    slope_origin_robust, intercept_origin_robust, r_value_origin_robust, p_value_origin_robust, stderr_origin_robust = robust_linregress(
+        x, y, force_origin=True)
     print("\nRobust Regression Through Origin:")
     print("Slope:", slope_origin_robust)
     print("Intercept:", intercept_origin_robust)
@@ -60,8 +65,10 @@ if __name__ == '__main__':
 
     plt.figure()
     plt.scatter(x, y, label='Data with outlier')
-    plt.plot(x, slope_origin_ols * x + intercept_origin_ols, color='blue', label='OLS Through Origin')
-    plt.plot(x, slope_origin_robust * x + intercept_origin_robust, color='purple', label='Robust Through Origin')
+    plt.plot(x, slope_origin_ols * x + intercept_origin_ols,
+             color='blue', label='OLS Through Origin')
+    plt.plot(x, slope_origin_robust * x + intercept_origin_robust,
+             color='purple', label='Robust Through Origin')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Regression Through Origin')
